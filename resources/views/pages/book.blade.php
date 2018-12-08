c568cbf9287953328e5f1d98e7e2e399420e9d9e
Živilė Valiuškaitė <zv2225@gmail.com>
2018-12-09 00:50:09 +0200

    katalogo funkcijos

--- resources/views/pages/book.blade.php
+++ resources/views/pages/book.blade.php
@@ -1,3 +1,65 @@
+<?php
+session_start();
+?>
+
+<head>
+    <title>Biblioteka</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        html, body {
+            background: linear-gradient(to bottom right, #A1B0AB, #E0CBA8);
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            margin: 0;
+            height: 100%;
+            background-attachment: fixed;
+        }
+
+        h2 {
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: bold;
+            font-size: 15px;
+        }
+
+        button
+        {
+            background-color: #A1B0AB;
+            color: black;
+            font-weight: bold;
+            font-size: 15px;
+            width: 100px;
+            border-radius: 12px;
+            font-family: 'Nunito', sans-serif;
+        }
+
+        button:hover {
+            background-color: #907D8D;
+            color: black;
+            font-family: 'Nunito', sans-serif;
+        }
+
+        input {
+            border-radius: 12px;
+            text-align: center;
+            font-family: 'Nunito', sans-serif;
+            width: 300px;
+        }
+        .dropdown2{
+            font-size: 20px;
+            font-weight: bold;
+        }
+
+    </style>
+</head>
+
+<html>
 <nav class="navbar navbar-inverse">
     <div class="container-fluid">
         <div class="navbar-header">
@@ -8,120 +70,26 @@
             <li class="{{Request::is('/ClientManagement')?'active':null }}"><a href="{{url('/ClientManagement')}}">Paskyros valdymas</a></li>
             <li><a href="#">Page 2</a></li>
             <li class="{{Request::is('/reports')?'active':null }}"><a href="{{url('/reports')}}">Ataskaitos</a></li>
-            <li class="{{Request::is('/UnitManagement')?'active':null }}"><a href="{{url('/UnitManagement')}}">Padaliniu valdymas</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-            <li class="{{Request::is('/')?'active':null}}"><a href="{{url('/')}}"><span class="glyphicon glyphicon-log-out"></span> Atsijungti</a></li>
+            <li class="{{Request::is('/logout')?'active':null}}"><a href="{{url('/logout')}}"><span class="glyphicon glyphicon-log-out"></span> Atsijungti</a></li>
         </ul>
     </div>
 </nav>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <title>Biblioteka</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<style>
-    * {box-sizing: border-box;}
-
-    body {
-        margin: 0;
-        font-family: Arial, Helvetica, sans-serif;
-    }
-
-    .topnav {
-        overflow: hidden;
-        background-color: #e9e9e9;
-    }
-
-    .topnav a {
-        float: left;
-        display: block;
-        color: black;
-        text-align: center;
-        padding: 14px 16px;
-        text-decoration: none;
-        font-size: 17px;
-    }
-
-    .topnav a:hover {
-        background-color: #ddd;
-        color: black;
-    }
-
-    .topnav a.active {
-        background-color: #2196F3;
-        color: white;
-    }
-
-    .topnav .search-container {
-        float: right;
-    }
-
-    .topnav input[type=text] {
-        padding: 6px;
-        margin-top: 8px;
-        font-size: 17px;
-        border: none;
-    }
-
-    .topnav .search-container button {
-        float: right;
-        padding: 6px 10px;
-        margin-top: 8px;
-        margin-right: 16px;
-        background: #ddd;
-        font-size: 17px;
-        border: none;
-        cursor: pointer;
-    }
-
-    .topnav .search-container button:hover {
-        background: #ccc;
-    }
-
-    @media screen and (max-width: 600px) {
-        .topnav .search-container {
-            float: none;
-        }
-        .topnav a, .topnav input[type=text], .topnav .search-container button {
-            float: none;
-            display: block;
-            text-align: left;
-            width: 100%;
-            margin: 0;
-            padding: 14px;
-        }
-        .topnav input[type=text] {
-            border: 1px solid #ccc;
-        }
-    }
-</style>
 <body>
-<div style='text-align:center'>
 
-        <h2>Pridėti naują knygą</h2>
-
-</div>
-</body>
-<body>
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <br>
-            <br>
-            <div style=text-align:left>
-
-                <h2>Pavadinimas</h2> <input type="text">
-                <h2>Autorius</h2> <input type="text">
-                <h2>Išleidimo data</h2> <input type="number">
-                <h2>Leidykla</h2> <input type="text">
+    <div class="col-md-6 col-md-offset-3">
+        <form class="" action="{{URL::to('/addBook')}}" method="post">
+            @csrf
+            <h3>Naujos knygos pridėjimas</h3><br>
+            <div>
+                <h2>Pavadinimas</h2> <input type="text" name="pavadinimas" required>
+                <h2>Autorius</h2> <input type="text" name="autorius" required>
+                <h2>Išleidimo data</h2> <input type="date" name="isleidimo_data" required/>
+                <h2>Leidykla</h2> <input type="text" name="leidykla" required>
                 <h2>Žanras</h2>   <div class="dropdown2">
-                    <select name="tipas">
+                    <select name="zanras">
                         <option value="1">Fantastika</option>
                         <option value="2">Romanas</option>
                         <option value="3">Novelė</option>
@@ -132,23 +100,17 @@
                         <option value="8">Detektyvas</option>
                     </select>
                 </div>
-                <h2>Puslapių kiekis</h2> <input type="number">
-                <h2>ISBN</h2> <input type="number">
 
+
+                <h2>Puslapių kiekis</h2> <input type="number" name="puslapiu_kiekis">
+                <h2>ISBN</h2> <input type="number" name="ISBN">
+                <h2>Knygų kiekis</h2> <input type="number" name="kiekis">
             </div>
-            <br>
-            <br>
-            <!-- DROP DOWN-->
-
-
-            <div style='text-align:right'>
-                <input type=button value='Patvirtinti'>
-            </div>
-            <br>
-        </div>
+<br>
+           <input type="hidden" name="_token" value="{{csrf_token()}}">
+            <button type=submit name="button">Patvirtinti</button>
+        </form>
     </div>
 </div>
 </body>
 </html>
-
-
